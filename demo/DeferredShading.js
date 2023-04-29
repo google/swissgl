ca9cc3f2fd6721b620094b67ced18458698b90ce
Alexander Mordvintsev <zzznah@gmail.com>
2023-04-29 09:53:06 +0200

    fewer lights

--- demo/DeferredShading.js
+++ demo/DeferredShading.js
@@ -4,11 +4,9 @@
   */
 
 class DeferredShading {
-    static Tags = ['3d'];
-    
     frame(glsl, params) {
         // draw objects
-        const gbuf = glsl({...params, Mesh:[64,128], Grid:[5,5,3],
+        const gbuf = glsl({...params, Mesh:[64,128], Grid:[4,4,3],
               Aspect:'fit', DepthTest:1, Clear:0, Inc:`
         varying vec3 normal, wldPos;`, VP:`
         vec3 surface_f(vec2 p) {
@@ -26,16 +24,17 @@ class DeferredShading {
             vec2 m = UV*vec2(Mesh)/4.0;
             FOut.rgb += (isoline(m.x)+isoline(m.y))*0.2; // color
             FOut1.xyz = normalize(normal);               // normal
-            FOut2.xyz = wldPos;                          // wldPos
+            FOut2 = vec4(wldPos, 1.0);                   // wldPos
         `}, {format:'rgba16f+depth', tag:'gbuf', layern:3});
 
-        // common lights passes code
-        const lightArgs = {...params, Face:'front', Grid:[9, 9, 7],
+        // common light passes code
+        const lightArgs = {...params, Face:'front', Grid:[9, 9, 1],
             Aspect:'fit', Blend:'s+d', DepthTest:'keep', Inc:`
             varying vec3 lightPos, lightColor;`, VP:`
             void vertex() {
                 lightPos = vec3(ID-Grid/2)*0.2;
-                lightPos.xy *= rot2(time*0.1+lightPos.z);
+                lightPos.xy *= rot2(time*0.1+lightPos.z)*(0.5+sin(time*0.7)*0.4);
+                lightPos.z = sin((lightPos.x+lightPos.y)*1.5+time*0.25)*0.75;
                 lightColor = hash(ID+1)*5.0;
                 VOut = wld2proj(lightPos+uv2sphere(UV)*lightR);
             }`};
@@ -43,16 +42,16 @@ class DeferredShading {
         // accumulate surface lights
         const light = glsl({...lightArgs, lightR:0.3, 
             Mesh:[32,64], Clear:0, gbuf, FP:`
-        vec4 color  = gbuf(ivec3(I,0));
-        if (color.a == 0.0) discard;
-        vec3 normal = gbuf(ivec3(I,1)).xyz;
-        vec3 wldPos = gbuf(ivec3(I,2)).xyz;
-        vec3 lightDir = lightPos-wldPos;
-        float r = length(lightDir)+1e-10;
-        float diff = max(dot(normal, lightDir/r), 0.0);
-        float att = color.a/(1.0+r*r*2e4);
-        FOut = vec4(color.rgb*lightColor*att*diff, 1.0);
-        `
+            vec4 wldPos = gbuf(ivec3(I,2));
+            if (wldPos.w==0.0) discard;
+            vec3 lightDir = lightPos-wldPos.xyz;
+            float r = length(lightDir)+1e-10;
+            if (r>lightR) discard;
+            vec3 color  = gbuf(ivec3(I,0)).rgb;
+            vec3 normal = gbuf(ivec3(I,1)).xyz;
+            float diff = max(dot(normal, lightDir/r), 0.0);
+            float att = 1.5*smoothstep(1.0,0.9,r/lightR) / (1.0+r*r*2e4);
+            FOut = vec4(color.rgb*lightColor*att*diff, 1.0);`
         }, {format:'rgba16f', tag:'light', depth:gbuf.depth});
         
         // add light source points

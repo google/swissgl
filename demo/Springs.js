bf5550c9b2e60802925bdf69280868807f13446c
Alexander Mordvintsev <zzznah@gmail.com>
2023-05-12 17:12:55 +0200

    cloth

--- demo/Springs.js
+++ demo/Springs.js
@@ -18,8 +18,8 @@ class Springs {
         const {glsl} = this;
         for (let i=0; i<2; ++i)
         this.points = glsl({FP:`
-            XY*rot2(-0.2)*0.5+vec2(0,0.0),0,(UV.x+UV.y)*4.0
-        `}, {size:[16, 16], format:'rgba32f', story:2, tag:'points'});
+            XY*rot2(PI+0.1)*0.6+vec2(0,0.3),0,(UV.x+UV.y)*10.0
+        `}, {size:[64, 64], format:'rgba32f', story:2, tag:'points'});
     }
 
     frame(glsl, {time}) {
@@ -27,8 +27,9 @@ class Springs {
 
         const next = glsl({time, ...this.params, points:points[0], prev:points[1], FP:`
             FOut = points(I);
+            if ((I.x%8 == 0 && I.y==0) || I == ivec2(ViewSize.x-1,0)) return;
             vec3 vel = FOut.xyz-prev(I).xyz;
-            vel.y -= 0.0001;
+            vel.y -= 0.0005;
             FOut += vec4(vel, waveRate/60.0);
             if (FOut.y<-0.8) {
                 FOut.x -= vel.x*0.8;
@@ -43,21 +44,24 @@ class Springs {
             }
             void fragment() {
                 vec4 p = FOut = Src(I);
+                if ((I.x%8 == 0 && I.y==0) || I == ivec2(ViewSize.x-1,0)) return;
                 vec4 acc = vec4(0);
                 ivec2 i, D = ViewSize;
                 float f0 = sin(p.w);
-                for (i.y=-2; i.y<=2; ++i.y) if (I.y+i.y>=0 && I.y+i.y<D.y)
-                for (i.x=-2; i.x<=2; ++i.x) if (I.x+i.x>=0 && I.x+i.x<D.x) {
+                const int R = 1;
+                for (i.y=-R; i.y<=R; ++i.y) if (I.y+i.y>=0 && I.y+i.y<D.y)
+                for (i.x=-R; i.x<=R; ++i.x) if (I.x+i.x>=0 && I.x+i.x<D.x) {
+                    if (abs(i.x)+abs(i.y)>1) continue;
                     if (i==ivec2(0)) continue;
                     vec4 p1 = Src(I+i);
                     float f1 = sin(p1.w);
-                    float f = 1.0 + 0.05*0.5*(f0+f1);
+                    float f = 1.0;// + 0.5*0.5*(f0+f1);
                     acc += relax(p.xyz, p1.xyz, f*length(vec2(i)/vec2(D)));
                 }
                 FOut.xyz = acc.xyz/acc.w;
                 if (FOut.y<-0.8) {
                     FOut.y = -0.8;
-                    FOut.x = mix(FOut.x, p.x, 0.8);
+                    FOut.x = mix(FOut.x, p.x, 0.9);
                 }
             }`}, next);
         glsl({next:next[0], FP:`next(I)`}, points)
@@ -69,10 +73,5 @@ class Springs {
         Blend: 'd*(1-sa)+s*sa',VP:`
         points(VID.xy).xy,0,1`,
         FP:`sqrt(isoline(UV.x*float(Mesh.x))+isoline(UV.y*float(Mesh.y)))`});
-
-        glsl({points:next[0], Grid:points[0].size, Aspect:'y', 
-        Blend: 'd*(1-sa)+s*sa',VP:`
-        VOut.xy = points(ID.xy).xy + XY*0.015;`,
-        FP:`smoothstep(1.0,0.9,length(XY))`});
     }
 }

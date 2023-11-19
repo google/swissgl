8975fde4bbc1e3a70504251e987d4eea88ac429e
jpaquim <joaopaquim@gmail.com>
2023-11-19 13:25:32 +0000

    Move demo list to index file

---
+++ demo/index.js
@@ -0,0 +1,47 @@
+import NeuralCA from './NeuralCA.js';
+import BitField from './BitField.js';
+import GameOfLife from './GameOfLife.js';
+import ParticleLife from './ParticleLife.js';
+import ParticleLenia from './ParticleLenia.js';
+import FancyLenia from './FancyLenia.js';
+import Spectrogram from './Spectrogram.js';
+import Physarum from './Physarum.js';
+import SurfaceNormals from './SurfaceNormals.js';
+import MeshGrid from './MeshGrid.js';
+import CubeDeform from './CubeDeform.js';
+import ColorCube from './ColorCube.js';
+import Shadowmap from './Shadowmap.js';
+import ParticleLife3d from './ParticleLife3d.js';
+import Torus4d from './Torus4d.js';
+import DeferredShading from './DeferredShading.js';
+import Springs from './Springs.js';
+import DotCamera from './DotCamera.js';
+import ReactionDiffusion from './ReactionDiffusion.js';
+import Physarum3d from './Physarum3d.js';
+import TextureSamplers from './TextureSamplers.js';
+import LargeLenia from './LargeLenia.js';
+
+export default [
+  NeuralCA,
+  DotCamera,
+  MeshGrid,
+  ParticleLife,
+  ParticleLife3d,
+  BitField,
+  TextureSamplers,
+  GameOfLife,
+  ParticleLenia,
+  FancyLenia,
+  LargeLenia,
+  Spectrogram,
+  Physarum,
+  Physarum3d,
+  SurfaceNormals,
+  CubeDeform,
+  ColorCube,
+  Shadowmap,
+  Torus4d,
+  DeferredShading,
+  Springs,
+  ReactionDiffusion,
+];
